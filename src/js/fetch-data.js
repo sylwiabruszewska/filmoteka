6de28a00139469e3aa613c18c0cae545426b1684
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-07-20 20:15:27 +0200

    FT-6  render movies on page

--- src/js/fetch-data.js
+++ src/js/fetch-data.js
@@ -13,3 +13,44 @@ async function fetchMovies(page) {
   console.log(fetchMovies.results);
   return fetchMovies.results;
 }
+
+async function renderMoviesCards(movies) {
+  const genres = await fetchGenres();
+
+  const markup = movies
+    .map(({ poster_path, title, name, genre_ids, release_date, first_air_date }) => {
+      const movieGenres = genre_ids
+        .map(genreId => {
+          const genre = genres.find(genre => genre.id === genreId);
+          return genre ? genre.name : null;
+        })
+        .filter(genreName => genreName)
+        .join(', ');
+
+      const releaseDate = (release_date || first_air_date || 'Brak danych').slice(0, 4);
+      const movieTitle = title ? title : name;
+      return `
+          <li class="movie-card">
+            <img class="movie-card__img" src="https://image.tmdb.org/t/p/w500${poster_path}" loading="lazy" alt="${movieTitle}" />
+            <h2>${movieTitle}</h2>
+            <p>${movieGenres}</p>
+            <p>${releaseDate}</p>
+          </li>
+        `;
+    })
+    .join('');
+
+  return galleryOfMovies.insertAdjacentHTML('beforeend', markup);
+}
+
+async function fetchGenres() {
+  const response = await fetch(`${BASE_URL}/${GENRE_LIST_PATH}?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.genres;
+}
+
+fetchMovies(page)
+  .then(renderMoviesCards)
+  .catch(error => console.error(error));
+
+export { fetchMovies, renderMoviesCards, fetchGenres };
