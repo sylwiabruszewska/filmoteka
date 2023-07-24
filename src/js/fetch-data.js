f3eef4f00cf03e49ecbfb7e9662ce5a542e8c75d
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-07-24 19:50:57 +0200

    FT-6 add page parameter to loadMovies function (for pagination)

--- src/js/fetch-data.js
+++ src/js/fetch-data.js
@@ -7,8 +7,6 @@ const BASE_URL = 'https://api.themoviedb.org/3';
 const MAIN_PAGE_PATH = '/trending/all/day';
 const GENRE_LIST_PATH = `/genre/movie/list`;
 
-let page = 1;
-
 async function fetchMovies(page) {
   const response = await fetch(`${BASE_URL}${MAIN_PAGE_PATH}?api_key=${API_KEY}&page=${page}`);
   const fetchMovies = await response.json();
@@ -52,7 +50,7 @@ async function fetchGenres() {
   return data.genres;
 }
 
-async function loadMovies() {
+async function loadMovies(page) {
   try {
     Notiflix.Block.arrows('.movie-gallery', {
       svgSize: '80px',
