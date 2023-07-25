1e5299edfdbe5755e5b6a10a5903e0ea5e6e722e
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-07-25 15:58:21 +0200

    add fetchMovieById function and render&open modal

--- src/js/fetch-data.js
+++ src/js/fetch-data.js
@@ -12,7 +12,6 @@ let page = 1;
 async function fetchMovies(page) {
   const response = await fetch(`${BASE_URL}${MAIN_PAGE_PATH}?api_key=${API_KEY}&page=${page}`);
   const fetchMovies = await response.json();
-  console.log(fetchMovies.results);
   return fetchMovies.results;
 }
 
@@ -20,7 +19,7 @@ async function renderMoviesCards(movies) {
   const genres = await fetchGenres();
 
   const markup = movies
-    .map(({ poster_path, title, name, genre_ids, release_date, first_air_date }) => {
+    .map(({ poster_path, title, name, genre_ids, id, release_date, first_air_date }) => {
       if (poster_path === null) {
         return;
       }
@@ -37,8 +36,8 @@ async function renderMoviesCards(movies) {
       const moviePoster =
         poster_path != null ? `https://image.tmdb.org/t/p/w500${poster_path}` : noMoviePoster;
       return `
-          <li class="movie-card">
-            <img class="movie-card__img" src="${moviePoster}" loading="lazy" alt="${movieTitle}" />
+          <li class="movie-card" data-id="${id}" data-modal-open>
+            <img class="movie-card__img" src="${moviePoster}" data-img="${moviePoster}" loading="lazy" alt="${movieTitle}" />
             <h2 class="movie-card__heading">${movieTitle}</h2>
             <span class="movie-card__caption">${movieGenres} | ${releaseDate}</span>
           </li>
@@ -46,7 +45,8 @@ async function renderMoviesCards(movies) {
     })
     .join('');
 
-  return galleryOfMovies.insertAdjacentHTML('beforeend', markup);
+  galleryOfMovies.insertAdjacentHTML('beforeend', markup);
+  addModalListenerFunction(); // nasłuchiwanie na kliknięcia po załadowaniu elementów
 }
 
 async function fetchGenres() {
@@ -67,13 +67,106 @@ async function loadMovies() {
       const movies = await fetchMovies(page);
       renderMoviesCards(movies);
       Notiflix.Block.remove('.movie-gallery');
-    }, 2000);
+    }, 1000);
   } catch (error) {
     console.error(error);
     Notiflix.Block.remove('.movie-gallery');
   }
 }
 
-loadMovies();
+///////////////////////////////////
+// FETCH MOVIE BY ID
+///////////////////////////////////
 
-export { fetchMovies, renderMoviesCards, fetchGenres };
+async function fetchMovieById(movieId) {
+  try {
+    const response = await fetch(`${BASE_URL}/movie/${movieId}?api_key=${API_KEY}`);
+    const responseObject = await response.json();
+    console.log(responseObject);
+    return responseObject;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+const addModalListenerFunction = () => {
+  const movieCards = document.querySelectorAll('.movie-card');
+  console.log('znalezione elementy LI:', movieCards.length);
+  addClickListenerToCards(movieCards);
+};
+
+// PIERWSZA WERSJA Z CONSOLE LOG
+// function addClickListenerToCards(cards) {
+//   cards.forEach(card => {
+//     card.addEventListener('click', async () => {
+//       const movieId = card.dataset.id;
+//       const movieData = await fetchMovieById(movieId);
+//       console.log('Movie ID:', movieData.id); // Wyświetl ID filmu w konsoli
+//       console.log('Poster Path:', movieData.poster_path);
+//       console.log('Votes:', movieData.vote_count);
+//       console.log('Popularity:', movieData.popularity);
+//       console.log('Original Title:', movieData.original_title || movieData.original_name);
+//       const genreNames = movieData.genres.map(genre => genre.name).join(', ');
+//       console.log('Genre:', genreNames);
+//       console.log('Description:', movieData.overview);
+//     });
+//   });
+// }
+
+function addClickListenerToCards(cards) {
+  const backdrop = document.querySelector('.backdrop');
+  const modalMovie = document.querySelector('.modal-movie');
+  const modalCloseButton = document.querySelector('[data-modal-close]');
+  const modalTitle = document.querySelector('.modal-movie__title');
+  const modalPoster = document.querySelector('.modal-movie__poster');
+  const modalVote = document.querySelector('.modal-movie__vote');
+  const modalVoteCount = document.querySelector('.modal-movie__vote-count');
+  const modalPopularity = document.querySelector('.modal-movie__popularity');
+  const modalOriginalTitle = document.querySelector('.modal-movie__original-title');
+  const modalGenre = document.querySelector('.modal-movie__genre');
+  const modalDescription = document.querySelector('.modal-movie__text');
+
+  cards.forEach(card => {
+    card.addEventListener('click', async () => {
+      const movieId = card.dataset.id;
+      const movieData = await fetchMovieById(movieId);
+
+      // renderowanie danych
+      modalTitle.textContent = movieData.title || movieData.name;
+      modalPoster.src = `https://image.tmdb.org/t/p/original/${movieData.poster_path}`;
+      modalVote.textContent = movieData.vote_average;
+      // modalVoteCount.textContent = movieData.vote_count;
+      modalPopularity.textContent = movieData.popularity;
+      modalOriginalTitle.textContent = movieData.original_title || movieData.original_name;
+      const genreNames = movieData.genres.map(genre => genre.name).join(', ');
+      modalGenre.textContent = genreNames;
+      modalDescription.textContent = movieData.overview;
+
+      // otwórz modal
+      backdrop.classList.remove('is-hidden');
+
+      // zamknij modal po kliknięciu na btn close
+      modalCloseButton.addEventListener('click', () => {
+        backdrop.classList.add('is-hidden');
+      });
+
+      resetMovieData();
+    });
+  });
+}
+
+function resetMovieData() {
+  modalTitle.textContent = '';
+  modalPoster.src = '';
+  modalVote.textContent = '';
+  modalVoteCount.textContent = '';
+  modalPopularity.textContent = '';
+  modalOriginalTitle.textContent = '';
+  modalGenre.textContent = '';
+  modalDescription.textContent = '';
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+  await loadMovies();
+});
