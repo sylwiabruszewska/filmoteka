38c0a0eba684f7bdc304fd924fd1f6ef139ac0e6
SebastianPrf <107537612+SebastianPrf@users.noreply.github.com>
2023-07-30 20:37:00 +0200

    Update library.js

--- src/library.js
+++ src/library.js
@@ -2,5 +2,5 @@ import './sass/main.scss';
 import './js/team';
 import './js/team-modal';
 import './js/movie-modal';
-// import './js/pagination';
+import './js/pagination';
 import './js/firebase/firebase-summary';
