f7fe9f1ab1000fe295cc64b8d83a4d093576dd8c
SebastianPrf <sebopar@gmail.com>
2023-08-01 22:12:00 +0200

    firebase
    go go

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './sass/main.scss';
 import './js/team';
 import './js/team-modal';
 import './js/pagination';
-import './js/search-movies';
+// import './js/search-movies';
 import './js/fetch-data';
 import './js/watch-add';
 import './js/queue-add';
