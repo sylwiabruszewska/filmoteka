4e4d59b6c1edf710c58c91b821d901f4fc2eb83c
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-08-01 13:05:52 +0200

    import library-load

--- src/library.js
+++ src/library.js
@@ -5,4 +5,5 @@ import './js/movie-modal';
 import './js/pagination';
 import './js/search-movies';
 import './js/fetch-data';
+import './js/library-load';
 // import './js/firebase/firebase-summary';
