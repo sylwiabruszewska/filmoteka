9010075b1579e8dccd9d6c0f288e7abe87a959fa
Mikołaj Marcinkowski <113313423+MikiMarcinkowski@users.noreply.github.com>
2023-08-02 01:36:14 +0200

    Delete index.js

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import './sass/main.scss';
-import './js/team';
-import './js/team-modal';
-import './js/movie-modal';
-import './js/pagination';
-// import './js/search-movies';
-import './js/fetch-data';
