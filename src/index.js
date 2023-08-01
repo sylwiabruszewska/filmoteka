5d3a1b911c06b25a90fd484d94bd2a6e11905876
mikus151 <mikus151@wp.pl>
2023-08-01 19:00:15 +0200

    firebase

--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import './js/team';
 import './js/team-modal';
 import './js/movie-modal';
 import './js/pagination';
-import './js/search-movies';
+// import './js/search-movies';
 import './js/fetch-data';
