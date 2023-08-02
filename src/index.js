00fc6b59c447dd24b81c2f006eb13476ccea3a44
Sylwia Bruszewska <sylwiabruszewska91@gmail.com>
2023-08-02 17:34:46 +0200

    import app.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import './sass/main.scss';
+import './js/app';
 import './js/team';
 import './js/team-modal';
 import './js/pagination';
-// import './js/search-movies';
+import './js/search-movies';
 import './js/fetch-data';
 import './js/local-storage';
-// import './js/watch-add';
-// import './js/queue-add';
-// import './js/buttons';
